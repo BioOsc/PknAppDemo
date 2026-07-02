20050c13f5e90cf97d8e5d1e2501ba0b4351ec71
Osciel Perea <bio_osc@hotmail.com>
2026-07-02 11:11:22 -0600

    Initial Commit

---
+++ PkmAppDemo/PkmAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PkmAppDemoApp.swift
+//  PkmAppDemo
+//
+//  Created by BioOsc on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct PkmAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
